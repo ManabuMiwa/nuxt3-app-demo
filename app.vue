baaa5242493d2c3ad2bb4dce6f448f31ccc84235
Manabu Miwa <manabu.miwa@w.ntt.com>
2022-03-03 15:44:04 +0900

    Add some script...

--- app.vue
+++ app.vue
@@ -2,9 +2,28 @@
   <div class="container">
     <div class="section">
       <h1 class="is-size-2">My Project</h1>
-      <button class="button is-primary mr-1">Click Me</button>
-      <button class="button is-primary mr-1">Click Me</button>
-      <button class="button is-primary">Click Me</button>
+      <button
+        class="button is-primary"
+        @click="handleClick"
+      >
+        Increment
+      </button>
+      <p class="is-size-3 mt-3">{{ myData }}</p>
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      myData: 0
+    }
+  },
+  methods: {
+    handleClick () {
+      this.myData++
+    }
+  }
+}
+</script>
