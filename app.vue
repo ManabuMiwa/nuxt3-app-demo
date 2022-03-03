819249eb809eb68b773b2711917f28423196e3f5
Manabu Miwa <manabu.miwa@w.ntt.com>
2022-03-03 15:21:54 +0900

    Initial Commit

---
+++ app.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="container">
+    <div class="section">
+      <h1 class="is-size-2">My Project</h1>
+      <button class="button is-primary mr-1">Click Me</button>
+      <button class="button is-primary mr-1">Click Me</button>
+      <button class="button is-primary">Click Me</button>
+    </div>
+  </div>
+</template>
